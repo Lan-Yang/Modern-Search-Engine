b6ee77d137c6d6d42c4a282869a9eff7d59955cc
Lan Yang <ireneyanglan@gmail.com>
2015-03-24 00:09:05 -0400

    input complete

--- query_answer.py
+++ query_answer.py
@@ -1,9 +1,19 @@
 import json
 import urllib
 
+question = raw_input('Input your question like "Who created [X]?", X is a book name or a company name: ')
+question = question.split()
+length = len(question)
+if (question[0].lower() != 'who' or question[1].lower() != 'created' or question[-1][-1] != '?'):
+	print 'Wrong input format!'
+	exit(1)
+
+search_term = ' '.join(question[2:])
+search_term = search_term[:-1]
+print 'search_term: ' + search_term
 api_key = open("api_key").read()
 service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
-search_term = "Microsoft"
+# search_term = "Microsoft"
 query = [{
   "/book/author/works_written": [{
     "a:name": None,
@@ -22,7 +32,6 @@ response = json.loads(urllib.urlopen(url).read())
 print '+ author:'
 for author in response['result']:
   print author['name']
-
 
 query = [{
   "/organization/organization_founder/organizations_founded": [{
