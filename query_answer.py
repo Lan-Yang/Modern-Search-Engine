0d0a77f50edcf8ffb002dde2bda2ae2b92e0cc30
Lan Yang <ireneyanglan@gmail.com>
2015-03-23 23:40:13 -0400

    original search result

--- query_answer.py
+++ query_answer.py
@@ -3,12 +3,42 @@ import urllib
 
 api_key = open("api_key").read()
 service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
-query = [{'author': None, 'name': 'Hobbit', 'type': '/book/author'}]
+search_term = "Microsoft"
+query = [{
+  "/book/author/works_written": [{
+    "a:name": None,
+    "name~=": search_term
+  }],
+  "id":   None,
+  "name": None,
+  "type": "/book/author"
+}]
 params = {
         'query': json.dumps(query),
         'key': api_key
 }
 url = service_url + '?' + urllib.urlencode(params)
 response = json.loads(urllib.urlopen(url).read())
-for planet in response['result']:
-  print planet['author']
+print '+ author:'
+for author in response['result']:
+  print author['name']
+
+
+query = [{
+  "/organization/organization_founder/organizations_founded": [{
+    "a:name": None,
+    "name~=": search_term
+  }],
+  "id": None,
+  "name": None,
+  "type": "/organization/organization_founder"
+}]
+params = {
+        'query': json.dumps(query),
+        'key': api_key
+}
+url = service_url + '?' + urllib.urlencode(params)
+response = json.loads(urllib.urlopen(url).read())
+print '+ founder:'
+for founder in response['result']:
+  print founder['name']
