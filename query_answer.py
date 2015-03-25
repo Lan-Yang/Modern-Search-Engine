2671292105fc3366dfefa5bb398c59ba436b9e72
Lan Yang <ireneyanglan@gmail.com>
2015-03-24 22:57:01 -0400

    part2 done

--- query_answer.py
+++ query_answer.py
@@ -1,19 +1,24 @@
 import json
 import urllib
 
+# User input question in the format "Who created [X]?"
 question = raw_input('Input your question like "Who created [X]?", X is a book name or a company name: ')
 question = question.split()
 length = len(question)
+result_list = []
+
+# Check input question's format
 if (question[0].lower() != 'who' or question[1].lower() != 'created' or question[-1][-1] != '?'):
 	print 'Wrong input format!'
 	exit(1)
 
+# Get [X]
 search_term = ' '.join(question[2:])
 search_term = search_term[:-1]
-print 'search_term: ' + search_term
 api_key = open("api_key").read()
 service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
-# search_term = "Microsoft"
+
+# Treat X as substring of a book name, search its author's name
 query = [{
   "/book/author/works_written": [{
     "a:name": None,
@@ -29,10 +34,26 @@ params = {
 }
 url = service_url + '?' + urllib.urlencode(params)
 response = json.loads(urllib.urlopen(url).read())
-print '+ author:'
-for author in response['result']:
-  print author['name']
 
+# Format output result in the format "A (as XXX) created <X1>, <X2>, ... and <Xn>."
+for author in response['result']:
+  tmp_list = []
+  tmp_str = ''
+  tmp_length = 0
+  name = author['name']
+  for book in author["/book/author/works_written"]:
+    tmp_list.append(book['a:name'])
+  tmp_length = len(tmp_list)
+  tmp_str = '%s (as Author) created <%s>' % (name, tmp_list[0])
+  tmp_length = tmp_length - 1
+  while (tmp_length > 1):
+    tmp_str = tmp_str + ', <%s>' % tmp_list[len(tmp_list) - tmp_length]
+    tmp_length = tmp_length - 1
+  if (tmp_length > 0):
+    tmp_str = tmp_str + ' and <%s>.' % tmp_list[len(tmp_list) - tmp_length]
+  result_list.append(tmp_str)
+
+# Treat X as substring of a company name, search its businessperson's name
 query = [{
   "/organization/organization_founder/organizations_founded": [{
     "a:name": None,
@@ -48,6 +69,28 @@ params = {
 }
 url = service_url + '?' + urllib.urlencode(params)
 response = json.loads(urllib.urlopen(url).read())
-print '+ founder:'
-for founder in response['result']:
-  print founder['name']
+
+# Format output result in the format "A (as XXX) created <X1>, <X2>, ... and <Xn>."
+for businessperson in response['result']:
+  tmp_list = []
+  tmp_str = ''
+  tmp_length = 0
+  name = businessperson['name']
+  for company in businessperson["/organization/organization_founder/organizations_founded"]:
+    tmp_list.append(company['a:name'])
+  tmp_length = len(tmp_list)
+  tmp_str = '%s (as BusinessPerson) created <%s>' % (name, tmp_list[0])
+  tmp_length = tmp_length - 1
+  while (tmp_length > 1):
+    tmp_str = tmp_str + ', <%s>' % tmp_list[len(tmp_list) - tmp_length]
+    tmp_length = tmp_length - 1
+  if (tmp_length > 0):
+    tmp_str = tmp_str + ' and <%s>.' % tmp_list[len(tmp_list) - tmp_length]
+  result_list.append(tmp_str)
+
+# Sort lines alphabetically by <Name>
+result_list.sort()
+
+# Display result
+for each_result in result_list:
+  print each_result
